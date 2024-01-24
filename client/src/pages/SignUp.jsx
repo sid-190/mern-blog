65603f95c87a4018d138312bf3642f324097630e
sid-190 <siddhirathod196@gmail.com>
2024-01-24 22:36:46 +0530

    create signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import { Button, Label, TextInput } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-      Sign Up
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/*left-side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span
+              className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500
+         to-pink-500 rounded-lg text-white"
+            >
+              MERN
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos dolore
+            consequatur, perspiciatis, enim, ad doloremque cumque recusandae et
+            consequuntur impedit architecto amet sit numquam expedita fugiat?
+            Commodi maiores similique culpa.
+          </p>
+        </div>
+        {/* right-side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your Username"/>
+              <TextInput
+              type="text"
+              placeholder="Username"
+              id='username'
+              />
+            </div>
+            <div>
+              <Label value="Your Email"/>
+              <TextInput
+              type="text"
+              placeholder="Email"
+              id='email'
+              />
+            </div>
+            <div>
+              <Label value="Your Password"/>
+              <TextInput
+              type="password"
+              placeholder="Password"
+              id='password'
+              />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">Sign Up</Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="sign-in" className="text-blue-500">Sign In</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
